330c26b667fed84cc2bae3a03fcd7c52ec17085d
bjarki15 <bjarki15@ru.is>
2018-02-23 18:18:32 +0000

    initial push

---
+++ ElevatorThreads_BjarkiH_LeifurP/src/com/ru/usty/elevator/ElevatorScene.java
@@ -0,0 +1,283 @@
+package com.ru.usty.elevator;
+
+import java.util.ArrayList;
+import java.util.concurrent.Semaphore;
+
+import javax.sound.midi.SoundbankResource;
+
+/**
+ * The base function definitions of this class must stay the same
+ * for the test suite and graphics to use.
+ * You can add functions and/or change the functionality
+ * of the operations at will.
+ *
+ */
+
+public class ElevatorScene {
+	
+	/**
+	 * !This is where you define semaphores!
+	 * When you call this semaphore,
+	 * put the class name it's reffering to
+	 * then a dot and call it, Don't just type globalSemaphore!
+	 */
+	//----Semaphores-----//
+	//There will be only one version of this running because it's static
+	//Pls cleanup after you new this !	
+	public static Semaphore globalSemaphore;
+	//that also exists mutex instead for Semaphore
+	public static Semaphore personCountMutex;
+	
+	public static Semaphore elevatorWaitMutex;
+	//------Semaphores------//
+	
+	public static boolean elevatorsMayDie;
+	public static ElevatorScene scene;
+	
+	//------threads--------//
+	private Thread elevatorThread = null;
+	//------threads-------//
+
+	//TO SPEED THINGS UP WHEN TESTING,
+	//feel free to change this.  It will be changed during grading
+	// ekki fyrir nedan 50 milliseconds -Bjarki
+	public static final int VISUALIZATION_WAIT_TIME = 500;  //milliseconds
+
+	private int numberOfFloors;
+	private int numberOfElevators;
+
+	ArrayList<Integer> personCount; //use if you want but
+									//throw away and
+									//implement differently
+									//if it suits you
+	ArrayList<Integer> exitedCount = null;
+	public static Semaphore exitedCountMutex;
+
+	//Base function: definition must not change
+	//Necessary to add your code in this one
+	public void restartScene(int numberOfFloors, int numberOfElevators) {
+		
+		elevatorsMayDie = true;
+		
+		/**for(Thread thread: elevatorThreads) {
+			if(thread != null) {
+				if(thread.isAlive()) {	DON'T DELETE, we will need it later
+					thread.join();
+				}
+			}
+		}*/
+		if(elevatorThread != null) {
+			if(elevatorThread.isAlive()) {
+				try {
+					elevatorThread.join();
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			
+		}
+		elevatorsMayDie = false;
+		
+		scene = this;
+		globalSemaphore = new Semaphore(0);// <- the first one that calls wait will be stopped
+		personCountMutex = new Semaphore(1);//<- the first one that calls wait gets through, which means: only one at a time
+		elevatorWaitMutex = new Semaphore(1);
+		
+		
+		
+		
+		/**
+		 * ATTENTION
+		 * This is not an acceptable way to create threads,
+		 * However, it's ok when you are testing. -Bjarki
+		 */
+		
+		elevatorThread = new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				
+				if(ElevatorScene.elevatorsMayDie) {
+					return; // this stops the program from running for an infinite time - Bjarki
+				}
+				
+				for(int i = 0; i < 16; i++) {
+					ElevatorScene.globalSemaphore.release();// signal - Bjarki
+				}
+				
+				
+			}
+			
+		});
+		elevatorThread.start();
+		/**
+		 * Important to add code here to make new
+		 * threads that run your elevator-runnables
+		 * 
+		 * Also add any other code that initializes
+		 * your system for a new run
+		 * 
+		 * If you can, tell any currently running
+		 * elevator threads to stop
+		 */
+
+		this.numberOfFloors = numberOfFloors;
+		this.numberOfElevators = numberOfElevators;
+
+		personCount = new ArrayList<Integer>();
+		for(int i = 0; i < numberOfFloors; i++) {
+			this.personCount.add(0);
+		}
+
+		if(exitedCount == null) {
+			exitedCount = new ArrayList<Integer>();
+		}
+		else {
+			exitedCount.clear();
+		}
+		for(int i = 0; i < getNumberOfFloors(); i++) {
+			this.exitedCount.add(0);
+		}
+		exitedCountMutex = new Semaphore(1);
+	}
+
+	//Base function: definition must not change
+	//Necessary to add your code in this one
+	public Thread addPerson(int sourceFloor, int destinationFloor) {
+		
+		//Go take a look at the person class when you want to add
+		//sourceFloor and destinationFloor into the thread
+		Thread thread = new Thread(new Person(sourceFloor, destinationFloor));
+		thread.start();
+		/**
+		 * Important to add code here to make a
+		 * new thread that runs your person-runnable
+		 * 
+		 * Also return the Thread object for your person
+		 * so that it can be reaped in the testSuite
+		 * (you don't have to join() yourself)
+		 */
+
+		ElevatorScene.scene.incrementNumberOfPeopleWaitingAtFloor(sourceFloor);
+		
+		return thread;  //this means that the testSuite will not wait for the threads to finish
+		//were returning the thread for the base system that will clean up after us - Bjarki
+	}
+
+	//Base function: definition must not change, but add your code
+	public int getCurrentFloorForElevator(int elevator) {
+
+		//dumb code, replace it!
+		return 1;
+	}
+
+	//Base function: definition must not change, but add your code
+	public int getNumberOfPeopleInElevator(int elevator) {
+		/**
+		 * The person class should be implemented to
+		 * get the value.. not elevator
+		 */
+		//dumb code, replace it!
+		switch(elevator) {
+		case 1: return 1;
+		case 2: return 4;
+		default: return 3;
+		}
+	}
+
+	//Base function: definition must not change, but add your code
+	public int getNumberOfPeopleWaitingAtFloor(int floor) {
+
+		return personCount.get(floor);
+	}
+	
+	public void decrementNumberOfPeopleWaitingAtFloor(int floor) {
+		try {
+			// We might need another mutex for floors
+			ElevatorScene.personCountMutex.acquire();
+				personCount.set(floor, (personCount.get(floor) -1));
+				ElevatorScene.personCountMutex.release();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} // wait
+		
+	}// added by Bjarki
+
+	public void incrementNumberOfPeopleWaitingAtFloor(int floor) {
+		try {
+			// We might need another mutex for floors
+			ElevatorScene.personCountMutex.acquire();
+				personCount.set(floor, (personCount.get(floor) +1));
+				ElevatorScene.personCountMutex.release();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} // wait
+		
+	}// added by Bjarki
+	//Base function: definition must not change, but add your code if needed
+	public int getNumberOfFloors() {
+		return numberOfFloors;
+	}
+
+	//Base function: definition must not change, but add your code if needed
+	public void setNumberOfFloors(int numberOfFloors) {
+		this.numberOfFloors = numberOfFloors;
+	}
+
+	//Base function: definition must not change, but add your code if needed
+	public int getNumberOfElevators() {
+		return numberOfElevators;
+	}
+
+	//Base function: definition must not change, but add your code if needed
+	public void setNumberOfElevators(int numberOfElevators) {
+		this.numberOfElevators = numberOfElevators;
+	}
+
+	//Base function: no need to change unless you choose
+	//				 not to "open the doors" sometimes
+	//				 even though there are people there
+	public boolean isElevatorOpen(int elevator) {
+
+		return isButtonPushedAtFloor(getCurrentFloorForElevator(elevator));
+	}
+	//Base function: no need to change, just for visualization
+	//Feel free to use it though, if it helps
+	public boolean isButtonPushedAtFloor(int floor) {
+
+		return (getNumberOfPeopleWaitingAtFloor(floor) > 0);
+	}
+
+	//Person threads must call this function to
+	//let the system know that they have exited.
+	//Person calls it after being let off elevator
+	//but before it finishes its run.
+	public void personExitsAtFloor(int floor) {
+		try {
+			
+			exitedCountMutex.acquire();
+			exitedCount.set(floor, (exitedCount.get(floor) + 1));
+			exitedCountMutex.release();
+
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	//Base function: no need to change, just for visualization
+	//Feel free to use it though, if it helps
+	public int getExitedCountAtFloor(int floor) {
+		if(floor < getNumberOfFloors()) {
+			return exitedCount.get(floor);
+		}
+		else {
+			return 0;
+		}
+	}
+
+
+}
---
+++ ElevatorThreads_BjarkiH_LeifurP/src/com/ru/usty/elevator/Person.java
@@ -0,0 +1,31 @@
+package com.ru.usty.elevator;
+
+public class Person implements Runnable {
+//Note that creating a runnable class means you have to specify everything that will run
+	int sourceFloor, destinationFloor;
+	
+	//This might not be the best way to get src and dst inside Person!
+	public Person(int sourceFloor, int destinationFloor) {
+		this.sourceFloor = sourceFloor;
+		this.destinationFloor = destinationFloor;
+	}
+	
+	@Override
+	public void run() {
+		
+		try {
+			ElevatorScene.elevatorWaitMutex.acquire();
+			 ElevatorScene.globalSemaphore.acquire(); //this is equivalent to a wait function
+			ElevatorScene.elevatorWaitMutex.acquire();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+		// is now available
+		ElevatorScene.scene.decrementNumberOfPeopleWaitingAtFloor(sourceFloor);
+		System.out.println("Person thread released");
+		
+		
+	}
+
+}
